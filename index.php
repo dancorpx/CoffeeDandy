94844a57a6a72fc521e2d3009eb9b3479ea50d15
Dan <dancorpx@gmail.com>
2020-05-02 22:21:15 +0800

    initial

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php 
+/* 
+The main template file
+*/
+?>
+<?php get_header() ?>
+
+<main class="container-fluid py-5">
+        <div class="row">
+            <div class="col-sm-8">
+            
+            <?php if( have_posts() ) : while ( have_posts()) : the_post(); ?>
+                <div>
+                    <a href="<?php the_permalink();?>">
+                        <h2><?php the_title(); ?></h2>
+                    </a>
+                    <p><?php echo get_the_date('F j, Y'); ?> by <a href=""><?php the_author(); ?></a></p>
+                    <div class="pb-2">
+                        <i class="fa fa-tags" aria-hidden="true"></i>
+                        <p class="d-inline"><?php the_tags('Tagged: ', ' ~ '); ?></p>
+                    </div>
+                    <p> <?php the_excerpt() ?> </p>
+                    <div class="mb-3">
+                        <a href="<?php the_permalink(); ?>">
+                            <?php _e('Read more...') ?>
+                        </a>
+                    </div>
+                </div>
+                <?php endwhile; else : ?>
+
+
+<p><?php _e('Sorry, no posts available'); ?></p>
+
+<?php
+endif;
+?>
+
+    <nav>
+        <ul class="nav">
+            <li><?php next_posts_link(); ?></li> &nbsp;
+            <li><?php previous_posts_link(); ?></li>
+        </ul>
+    </nav>
+</div>
+
+        <aside class="col-sm-4">
+           <?php get_sidebar(); ?>    
+        </aside>
+    </div> <!--closing row-->
+</main> <!--closing container-->
+
+<!-- Footer section -->
+<?php get_footer() ?>
